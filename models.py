8173234e05317596ef3137ff7dc2f7e74585ef9e
fireplacetv <fireplacetv@gmail.com>
2017-04-05 13:46:25 -0700

    adding new models, phase 1

--- models.py
+++ models.py
@@ -29,3 +29,39 @@ class Word(db.Model):
 			'chinese': self.chinese,
 			'pinyin': self.pinyin
 		}
+
+class User(db.Model):
+	__tablename__ = "users"
+	uid = db.Column(db.Integer, primary_key=True)
+	email = db.Column(db.String(60), unique=True)
+	password = db.Column(db.String(120))
+
+	def __init__(self, email, pssword):
+		self.email = email
+		self.password = password
+
+	def serialize(self):
+		return {
+			'email': self.email,
+			'password': self.password
+		}
+
+class Deck(db.Model):
+	__tablename__ = "decks"
+	did = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(60))
+	owner = db.Column(db.Integer, db.ForeignKey('users.uid'))
+
+	def __init__(self, name, ownerId):
+		self.name = name
+		self.owner = ownerId
+
+class DeckCards(db.Model):
+	__tablename__ = "deckcards"
+	dcid = db.Column(db.Integer, primary_key = True)
+	did = db.Column(db.Integer, db.ForeignKey('decks.did'))
+	wid = db.Column(db.Integer, db.ForeignKey('words.wid'))
+
+	def __init__(self, deckId, wordId):
+		self.did = deck
+		self.wid = word
